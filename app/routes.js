8990dc6333daff3bdedcc11c54856aa0a0fe1382
emilio.caamano@accenture.com <emilio@C11-1QYIR7IQFRP.dir.svc.accenture.com>
2022-08-29 17:04:11 -0300

    API integration

--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,10 @@
 // const controller = require('./controllers/controller');
 const { healthCheck } = require('./controllers/healthCheck');
+const { weets } = require('./controllers/weets');
 
 exports.init = app => {
   app.get('/health', healthCheck);
+  app.get('/weets', weets);
   // app.get('/endpoint/get/path', [], controller.methodGET);
   // app.put('/endpoint/put/path', [], controller.methodPUT);
   // app.post('/endpoint/post/path', [], controller.methodPOST);
