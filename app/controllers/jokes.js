91152788ee431c146d02203228188c1881edf352
emilio.caamano@accenture.com <emilio@C11-1QYIR7IQFRP.dir.svc.accenture.com>
2022-09-05 10:42:46 -0300

    Fix: PR fixes

--- app/controllers/jokes.js
+++ app/controllers/jokes.js
@@ -1,6 +1,10 @@
 const jokes = require('../services/jokes');
 
 exports.jokes = async (req, res) => {
-  const joke = await jokes.getJokes();
-  res.status(200).send(joke);
+  try {
+    const joke = await jokes.getJokes();
+    res.status(200).send(joke);
+  } catch (error) {
+    res.status(404).send(error);
+  }
 };
