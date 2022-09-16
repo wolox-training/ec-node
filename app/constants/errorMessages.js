b882c698e1623dcc6cf966d9769c901b3b96c4c4
emilio.caamano@accenture.com <emilio@C11-1QYIR7IQFRP.dir.svc.accenture.com>
2022-09-16 12:38:14 -0300

    Fix: Error fixes}

--- app/constants/errorMessages.js
+++ app/constants/errorMessages.js
@@ -1,6 +1,7 @@
 const errorMsg = {
   apiError: 'Error: falied to connect with jokes api.',
-  userError: 'Error: failed to create user.'
+  userError: 'Error: failed to create user.',
+  encriptedError: 'Error: failed to encrypt the password'
 };
 
 module.exports = errorMsg;
--- app/helpers/helpers.js
+++ app/helpers/helpers.js
@@ -1,13 +1,15 @@
 const bcryptjs = require('bcryptjs');
 const config = require('../../config');
 const logger = require('../logger');
+const errorMsg = require('../constants/errorMessages');
+const { defaultError } = require('../errors');
 
-exports.encryptPassword = (password, next) => {
+exports.encryptPassword = password => {
   try {
     const encryptedPassword = bcryptjs.hashSync(password, Number(config.auth.rounds));
     return encryptedPassword;
   } catch (error) {
     logger.error(error.message);
-    return next(error);
+    throw defaultError(errorMsg.userError);
   }
 };
