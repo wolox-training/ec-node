249b388d7d41bd33816ae62c13b9f6e407ae94a0
emilio.caamano@accenture.com <emilio@C11-1QYIR7IQFRP.dir.svc.accenture.com>
2022-09-19 18:26:33 -0300

    Feat: Test

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -5,9 +5,11 @@ const helper = require('../helpers/helpers');
 
 exports.signUp = async (req, res, next) => {
   try {
+    console.log(req);
     const password = helper.encryptPassword(req.body.password);
     const userObject = req.body;
     const userCreated = varParse.nameParse(userObject, password);
+    console.log(userCreated);
     await userService.createUser(userCreated);
     logger.info('User created: ', userCreated);
     res.status(201).send(userCreated);
