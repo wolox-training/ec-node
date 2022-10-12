9ecd5ca783dbe03c1884f5c24accc3c5ebaadb1f
emilio.caamano@accenture.com <emilio@C11-1QYIR7IQFRP.dir.svc.accenture.com>
2022-10-12 15:16:34 -0300

    PR: Fixes

--- test/mockWeet.js
+++ test/mockWeet.js
@@ -10,6 +10,8 @@ exports.weetFailed = {
   internalCode: DEFAULT_ERROR,
   message: errorMsg.userFindError
 };
-exports.responseSuccess =
+exports.mockToken =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyT25saW5lIjp7ImlkIjoxLCJmaXJzdF9uYW1lIjoiSnVhcXVpbiIsImxhc3RfbmFtZSI6ImRzYWRzIiwiZW1haWwiOiJFbWlsaW9Ad29sb3guY29tIiwicGFzc3dvcmQiOiIkMmEkMTAkbkZVbjdrYWFUR2RHQ2sxMnlQbVFVT3I0aWhZbEpBVXp1b1dVVE9uSXdkbmNFSHlwRWxVRUciLCJhZG1pbiI6ZmFsc2V9LCJpYXQiOjE2NjU1MjIyNDgsImV4cCI6MTY2NTYwODY0OH0.mYRcs2stEEc-LyJXHb537V8Af_5cYI2WF9o1nHpbt0c';
-exports.responseFailed = 'asdfsad';
+exports.responseFailed = {
+  content: 'Chuck Norris decided to sell his poop as a food product ... we know it now as wheaties.'
+};
